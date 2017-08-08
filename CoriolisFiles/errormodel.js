b12bc84f23c0eef35d8c1d53c4b480a87940f8fb
Priceisright204 <kprice204@gmail.com>
2017-08-07 21:44:37 -0400

    Added the graph, and improved model accuracy!

--- CoriolisFiles/errormodel.js
+++ CoriolisFiles/errormodel.js
@@ -7,6 +7,11 @@ var height_thrown = 3;
 //var mouseover = false; //input values on first mouseover. Only first.
 var allatonce = false; //used so that some functions can change all the values, and submit only once.
 
+Decimal.set({ precision: 30 })
+
+var PI = Decimal.acos(-1)
+
+
 function setunits(setting) {
 	if(setting=="m")
 	{ var changeto="metric" }
@@ -107,7 +112,10 @@ function setfields() {
 	//otherwise do nothing
 }
 
-function calc_error(diameter, height_start, gs, height_thrown) {
+//This function version (not used) is prior to adding the decimal.js module.
+//Look through the code if you want a more straightforward understanding
+//of what formulas are being used in the current, more convoluted, function.
+function calc_error_original(diameter, height_start, gs, height_thrown) {
 	
 	radius = diameter/2
 	
@@ -264,6 +272,168 @@ function calc_error(diameter, height_start, gs, height_thrown) {
 	return answers	
 }
 
+function calc_error(diameter, height_start, gs, height_thrown) {
+	
+	radius = Decimal(diameter/2)
+	
+		var accel_earth
+	
+	//Get acceleration at floor.
+	if(units=="ft")
+	{	
+		accel_earth = 32.174; // ft/s/s
+	} else
+	{
+		accel_earth = 9.80665; // m/s/s, exact conventional standard
+	}
+	
+	var g_accel = accel_earth * gs;
+	
+	//CALCULATE!!! /////////////////////////////////////////
+	////////////////////////////////////////////////////////
+	
+	var standingvelocity = Decimal.sqrt( g_accel * radius )
+	
+	//Get variables and perform calculations here.
+	//Accurate up to about 15 decimals (17?)
+	
+	//get forces on the ball
+	//a = omega^2 * r  eqn with rotational velocity
+	var omega = Decimal.sqrt( g_accel / radius );
+
+	//var height_start = height_person * (2/3);
+	
+	var r_onball = radius - height_start;
+
+	var g_onball = omega*omega*r_onball;
+	
+	
+	
+	
+	//Now get the starting velocities.
+	var start_v_y = Decimal.sqrt( 2 * accel_earth * height_thrown ) //calculated comparing to Earth's gravity
+	var start_v_x = -1 * Decimal.sqrt( g_onball * r_onball ) //-1 b/c station is rotating clockwise
+
+		//Now find intersection with the circle.
+	var slope_ball = start_v_y / start_v_x
+	
+	var sqroot1 = Decimal.sqrt( Decimal.pow(slope_ball,2).mul(Decimal.pow(r_onball,2)).sub((Decimal(1).add(Decimal.pow(slope_ball,2))).mul( Decimal.pow(r_onball,2).sub(Decimal.pow(radius,2)  )) ) )
+	
+	//var x_1 = ( (slope_ball * r_onball) + sqroot1 ) / ( 1 + Math.pow(slope_ball,2)) // +
+	var x_2 = ( Decimal(slope_ball * r_onball).sub( sqroot1 )).div( Decimal(1).add( Decimal.pow(slope_ball,2)) ) // -
+
+	//WARNING: THIS DOES NOT GIVE YOU THE CORRECT SIGN
+	//but it gives the absolute values that go with X.
+	//var y_1_abs = Decimal.sqrt(Math.pow(radius,2) - Math.pow(x_1,2) ) 
+	var y_2_abs = Decimal.sqrt(Math.pow(radius,2) - Math.pow(x_2,2) )
+	
+	//Now use the quadratic again for Y to get the sign.
+
+	//var sqroot2 = Math.sqrt( Math.pow(r_onball,2) - ( 1 + Math.pow(slope_ball,2) ) * ( Math.pow(r_onball,2) - Math.pow(radius,2) * Math.pow(slope_ball,2) ) )
+
+	
+	var sqroot2 = Decimal.sqrt( Decimal.pow(r_onball,2).sub(( Decimal(1).add(Decimal.pow(slope_ball,2) ) ).mul(Decimal.pow(r_onball,2).sub(Decimal.pow(radius,2).mul(Decimal.pow(slope_ball,2))))) )
+		
+	//Apparently the (-) from the Y eqn goes with the (+) in the X eqn. Just to be confusing.
+	//var y_1 = ( (-1 * r_onball) - sqroot2 ) / ( 1 + Math.pow(slope_ball,2) ) // -
+	//var y_2 = ( (-1 * r_onball) + sqroot2 ) / ( 1 + Math.pow(slope_ball,2) ) // +
+
+	var y_2 = ( new Decimal(-1 * r_onball).add( sqroot2 ) ).div( new Decimal(1).add(Decimal.pow(slope_ball,2) ) ) // +
+	
+	//(X2, Y2) is the point that we want!
+	
+	var start_vtotal = Decimal.sqrt( Decimal.pow(start_v_x,2).add(Decimal.pow(start_v_y,2) ) )
+	
+	//Get the time.
+	var time = Decimal.sqrt( Decimal.pow(x_2,2).add(( y_2.add(r_onball )).pow(2) )).div( start_vtotal )
+	
+	//radial angle swept by the person
+	var theta_traversed_person = omega.mul( time )
+	var initial_angle_person = (Decimal(3).mul(PI)).div(2)
+	var final_angle_person = initial_angle_person.sub(theta_traversed_person) //minus b/c clockwise rotation
+	
+	//final position of person
+	//fortunately Javascript uses radians, not degrees
+	var xf_person = radius.mul(Decimal.cos(final_angle_person) )
+	var yf_person = radius.mul(Decimal.sin(final_angle_person) )
+	
+	var x_difference = xf_person.sub( x_2 )
+	var y_difference = yf_person.sub( y_2 )
+	
+	var total_difference = Decimal.sqrt( Decimal.pow(x_difference,2).add(Decimal.pow(y_difference,2) ) )
+
+	
+	
+	
+	
+	
+	///MAX HEIGHT/////////////////////////////////////
+	//////////////////////////////////////////////////
+
+	//a maximization function.
+
+	//Starting values
+	var test_x = start_vtotal*(time/10) //Starting test increment
+	var direction = -1  //iterate leftwards to start.
+	var maxvalue = height_start
+
+	var current_x = 0    //ball goes left. Negative.
+	var current_y = 0
+	var current_value = 0
+	var disttocenter = 0
+	var lastvalue = 0
+	var timesinarow = 0
+	
+	for(i=0; i<1000; i++)
+	{
+		current_x = current_x + (test_x * direction)
+		
+		//y = mx + b
+		current_y = (slope_ball * current_x) - r_onball
+		
+		//pythagoras, to find distance to ball from center of station
+		disttocenter = Decimal.sqrt( Math.pow(current_x,2) +Math.pow(current_y,2) )
+		
+		current_value = radius - disttocenter
+		
+		if(current_value<lastvalue)
+		{  //if the values are decreasing
+			//then it's the wrong direction.
+			direction = direction * -1;
+			test_x = test_x / 2;
+		}
+
+		if(current_value > maxvalue)
+		{ maxvalue = current_value  }
+		
+		if( Math.abs(current_value - lastvalue) < .0001 )
+		{ timesinarow += 1	}
+		else
+		{ timesinarow = 0 }
+		
+		if(timesinarow == 5)
+		{
+			break;
+		}
+		
+		lastvalue = current_value;
+	}
+	
+	
+	var answers = {
+		maxvalue: maxvalue,
+		g_accel: g_accel,
+		radius: radius,
+		standingvelocity: standingvelocity,
+		start_v_y:start_v_y,
+		omega: omega,
+		total_difference: total_difference,
+		time: time,
+		accel_earth: accel_earth
+	}
+	
+	return answers	
+}
 
 
 //What happens after the person pushes the submit button...
@@ -298,7 +468,7 @@ function submit() {
 	var expectedheight = height_start + height_thrown
 	document.getElementById("expectedheight").innerHTML = round( expectedheight )
 	document.getElementById("expectedheightunits").innerHTML = 	"&nbsp;" + units
-	document.getElementById("expectedtime").innerHTML = round( Math.sqrt( 2 * expectedheight / answers.accel_earth ) + (answers.start_v_y / answers.accel_earth)  )
+	document.getElementById("expectedtime").innerHTML = round( Decimal.sqrt( 2 * expectedheight / answers.accel_earth ).add(answers.start_v_y / answers.accel_earth)  )
 	document.getElementById("expectedtimeunits").innerHTML = "&nbsp;s"
 
 	
