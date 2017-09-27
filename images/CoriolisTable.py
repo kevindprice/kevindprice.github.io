ad9dcdf6b27e5c678fd8a4e8edfed76438abe15e
Priceisright204 <kprice204@gmail.com>
2017-09-27 18:08:50 -0400

    This *might* be the final version... but it still has to be edited by Erin

--- images/CoriolisTable.py
+++ images/CoriolisTable.py
@@ -6,11 +6,153 @@ import numpy as np
 import matplotlib.ticker as mticker
 
 from decimal import *
-getcontext().prec = 28
+
 #The decimal package is necessary to use higher-accuracy numbers.
 #Fifteen decimal places is not sufficient; otherwise there are
-#errors in the output at about diameter > 1000000.
+#errors in the output at about diameter > 100000.
 
+getcontext().prec = 28     #28 decimal places
+
+
+"""Performs one calculation."""
+def calc_diff(diameter, height_start, gs, heightthrown, units = "metric"):
+    if units == "metric":
+        accel_earth = Decimal("9.80665")
+    else:
+        accel_earth = Decimal("32.174")
+
+    g_accel = accel_earth * gs              #Gravitational acceleration
+    
+    radius = diameter / Decimal("2")
+    omega = Decimal.sqrt( g_accel / radius )   #rotation rate (radians / second)
+    r_oncoin = radius - height_start           #radius "to the coin"
+    g_oncoin = omega*omega*r_oncoin            #gravity "on the coin"
+    
+    #starting velocity components
+    start_v_y = Decimal.sqrt ( Decimal("2") * accel_earth * heightthrown )
+    start_v_x = Decimal("-1") * Decimal.sqrt( g_oncoin * r_oncoin)
+    start_vtotal = Decimal.sqrt( start_v_x**2 + start_v_y**2 )
+    
+    slope_coin = start_v_y / start_v_x
+    
+    #Now find the landing coordinates
+    sqroot1 = Decimal.sqrt( slope_coin**2 * r_oncoin**2 - ((Decimal("1") + slope_coin**2) * ( r_oncoin**2 - radius**2 )) )
+    
+    x_2 = ( ( slope_coin * r_oncoin) - sqroot1 ) / ( Decimal("1") + slope_coin**2)
+    
+    sqroot2 = Decimal.sqrt( r_oncoin**2 - (Decimal("1") + slope_coin**2 ) * ( r_oncoin**2 - radius**2 * slope_coin**2 ) )
+    
+    y_2 = ( (Decimal("-1") * r_oncoin) + sqroot2 ) / (Decimal("1") + slope_coin**2 )
+    
+    #Time in the air
+    time = Decimal.sqrt( x_2**2 + ( y_2 + r_oncoin )**2 ) / start_vtotal
+    
+    #Angle person has passed
+    theta_traversed_person = omega*time
+    initial_angle_person = (Decimal("3") * pi() ) / Decimal("2")
+    final_angle_person = initial_angle_person - theta_traversed_person
+    
+    #Person's final coordinates
+    xf_person = radius * cos(final_angle_person)
+    yf_person = radius * sin(final_angle_person)
+    
+    #Find difference
+    x_difference = xf_person - x_2
+    y_difference = yf_person - y_2
+    
+    total_difference = Decimal.sqrt( x_difference**2 + y_difference**2 )
+    
+    return float(total_difference*Decimal("100"))
+    
+def run():
+    answers = []
+    loop = 0
+    
+    #Starting values
+    diameter = Decimal("6")
+    height_start = Decimal("1.219")
+    gs = Decimal("1")
+    heightthrown = Decimal("0.914")
+
+    #Loop through many values to generate data.
+    while diameter < 100000001:
+
+        try: #Perform one calculation. Catch errors.
+            currenttuple = (float(diameter), calc_diff(diameter, height_start, gs, heightthrown, units = "imperial") )
+            
+            answers.append(currenttuple)
+
+        except InvalidOperation:  #if I purposely decrease the number of decimals
+            pass                  #to show how glitchy it is
+                                  #then it will produce this error.
+                                  #Otherwise this is unnecessary.
+        
+        loop+=1
+
+        #Stepping over 0.1 increments to a billion is unnecessary and wasteful.
+        #I need fewer and fewer datapoints as the diameter increases.
+        #I am generating a log graph.
+        
+        if diameter < 99.9: #A hacky solution to an imprecise decimal comparison.
+            diameter += Decimal("0.1")
+            
+        elif diameter < 1000:
+            diameter += Decimal("1.0")
+            
+        elif diameter < 10000:
+            diameter += Decimal("10.0")
+            
+        elif diameter < 100000:
+            diameter += Decimal("100.0")
+            
+        elif diameter < 1000000:
+            diameter += Decimal("1000.0")
+        
+        elif diameter < 10000000:
+            diameter += Decimal("10000.0")
+
+        else:
+            diameter += Decimal("100000.0")
+
+    #Generate a dataframe with all the values
+    output = pd.DataFrame(answers, columns = ["diameter", "error"])
+    output.to_csv("Error Table.csv", index=False)
+
+    #Generate the graph
+    fig, ax = plt.subplots()
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+    plt.scatter(output["diameter"],output["error"])
+    
+    #Set up y-axis labels
+    ax.yaxis.set_major_formatter(mticker.ScalarFormatter())
+    ax.yaxis.get_major_formatter().set_scientific(False)             #no exponential format
+    ax.yaxis.get_major_formatter().set_useOffset(False)              #remove awkward offset in corner
+    ax.yaxis.set_major_formatter(mticker.FormatStrFormatter('%.1f')) #show labels to 1 decimal
+
+    #x-axis
+    ax.xaxis.set_major_formatter(mticker.ScalarFormatter())
+    ax.xaxis.get_major_formatter().set_scientific(False)
+    ax.xaxis.get_major_formatter().set_useOffset(False)
+
+    ax.get_xaxis().set_major_formatter(
+        mticker.FuncFormatter(lambda x, p: format(int(x), ',')))     #Add commas
+
+    fig.canvas.draw()
+    labels = [item.get_text() for item in ax.get_yticklabels()]
+    labels = [label.replace(".0","") for label in labels]            #Only need decimal for "0.1".
+    ax.set_yticklabels(labels)                                       #(remove ".0")
+    
+    plt.xlabel("Station Diameter (Meters)")
+    plt.ylabel("Distance \"Away\" (Centimeters)")
+    plt.suptitle("Distance \"Away\" A Tossed Coin Would Land in Artificial Gravity")
+
+    #Rotate the labels
+    plt.setp(ax.xaxis.get_majorticklabels(),rotation=40,horizontalalignment='right')
+    plt.subplots_adjust(bottom=0.22)
+    #plt.show()  #for debugging
+    plt.savefig("CoriolisGraph.png")
+    plt.close()
 
 
 #These functions are higher-accuracy than the standard math package.
@@ -86,143 +228,6 @@ def sin(x):
     return +s
 
 
-def calc_diff(diameter, height_start, gs, heightthrown, units = "metric"):
-    if units == "metric":
-        accel_earth = Decimal("9.80665")
-    else:
-        accel_earth = Decimal("32.174")
-
-    g_accel = accel_earth * gs
     
-    radius = diameter / Decimal("2")
-    
-    omega = Decimal.sqrt( g_accel / radius )
-    r_onball = radius - height_start
-    g_onball = omega*omega*r_onball
-    
-    start_v_y = Decimal.sqrt ( Decimal("2") * accel_earth * heightthrown )
-    start_v_x = Decimal("-1") * Decimal.sqrt( g_onball * r_onball)
-    
-    #expectedheight = height_start + heightthrown
-    
-    slope_ball = start_v_y / start_v_x
-    
-    sqroot1 = Decimal.sqrt( slope_ball**2 * r_onball**2 - ((Decimal("1") + slope_ball**2) * ( r_onball**2 - radius**2 )) )
-    
-    #x_1 = ( ( slope_ball * r_onball) + sqroot1 ) / ( 1 + math.pow(slope_ball,2))
-    x_2 = ( ( slope_ball * r_onball) - sqroot1 ) / ( Decimal("1") + slope_ball**2)
-    
-    #y_1_abs = Decimal.sqrt( math.pow(radius,2) - math.pow(x_1,2) )
-    y_2_abs = Decimal.sqrt( radius**2 - x_2**2 )
-    
-    sqroot2 = Decimal.sqrt( r_onball**2 - (Decimal("1") + slope_ball**2 ) * ( r_onball**2 - radius**2 * slope_ball**2 ) )
-    
-    #y_1 = ( (-1 * r_onball) - sqroot2 ) / (1 + math.pow(slope_ball,2) )
-    y_2 = ( (Decimal("-1") * r_onball) + sqroot2 ) / (Decimal("1") + slope_ball**2 )
-    
-    start_vtotal = Decimal.sqrt( start_v_x**2 + start_v_y**2 )
-    
-    time = Decimal.sqrt( x_2**2 + ( y_2 + r_onball )**2 ) / start_vtotal
-    
-    theta_traversed_person = omega*time
-    initial_angle_person = (Decimal("3") * pi() ) / Decimal("2")
-    final_angle_person = initial_angle_person - theta_traversed_person
-    
-    xf_person = radius * cos(final_angle_person)
-    yf_person = radius * sin(final_angle_person)
-    
-    x_difference = xf_person - x_2
-    y_difference = yf_person - y_2
-    
-    total_difference = Decimal.sqrt( x_difference**2 + y_difference**2 )
-    
-    return float(total_difference*Decimal("100"))
-    
-def run():
-    answers = []
-    loop = 0
-    
-    diameter = Decimal("6")
-    height_start = Decimal("1.219")
-    gs = Decimal("1")
-    heightthrown = Decimal("0.914")
-
-    
-    while diameter < 100000001:
-
-        try:
-            currenttuple = (float(diameter), calc_diff(diameter, height_start, gs, heightthrown, units = "imperial") )
-            
-            answers.append(currenttuple)
-
-        except InvalidOperation:  #if I purposely decrease the number of decimals
-            pass                  #to show how glitchy it is
-                                  #then it will produce this error.
-        
-        
-        loop+=1
-
-        #Stepping over 0.1 increments to a billion is unnecessary and wasteful.
-        #I need fewer and fewer datapoints as the diameter increases.
-        #I am generating a log graph.
-        
-        if diameter < 99.9: #A hacky solution to an imprecise decimal comparison.
-            diameter += Decimal("0.1")
-            
-        elif diameter < 1000:
-            diameter += Decimal("1.0")
-            
-        elif diameter < 10000:
-            diameter += Decimal("10.0")
-            
-        elif diameter < 100000:
-            diameter += Decimal("100.0")
-            
-        elif diameter < 1000000:
-            diameter += Decimal("1000.0")
-        
-        elif diameter < 10000000:
-            diameter += Decimal("10000.0")
-
-        else:
-            diameter += Decimal("100000.0")
-
-    output = pd.DataFrame(answers, columns = ["diameter", "error"])
-    output.to_csv("Error Table.csv", index=False)
-
-
-    fig, ax = plt.subplots()
-    ax.set_yscale('log')
-    ax.set_xscale('log')
-    plt.scatter(output["diameter"],output["error"])
-
-    ax.yaxis.set_major_formatter(mticker.ScalarFormatter())
-    ax.yaxis.get_major_formatter().set_scientific(False)
-    ax.yaxis.get_major_formatter().set_useOffset(False)
-    ax.yaxis.set_major_formatter(mticker.FormatStrFormatter('%.1f'))
-
-    ax.xaxis.set_major_formatter(mticker.ScalarFormatter())
-    ax.xaxis.get_major_formatter().set_scientific(False)
-    ax.xaxis.get_major_formatter().set_useOffset(False)
-
-    ax.get_xaxis().set_major_formatter(
-        mticker.FuncFormatter(lambda x, p: format(int(x), ',')))
-
-    fig.canvas.draw()
-    labels = [item.get_text() for item in ax.get_yticklabels()]
-    labels = [label.replace(".0","") for label in labels]
-    ax.set_yticklabels(labels)
-        
-    plt.xlabel("Station Diameter (Meters)")
-    plt.ylabel("Distance \"Away\" (Centimeters)")
-    plt.suptitle("Distance \"Away\" A Coin Would Land in Artificial Gravity")
-
-    plt.setp(ax.xaxis.get_majorticklabels(),rotation=40,horizontalalignment='right')
-    plt.subplots_adjust(bottom=0.22)
-    #plt.show()
-    plt.savefig("CoriolisGraph.png")
-    plt.close()
-
-
 if __name__=="__main__":
     run()
