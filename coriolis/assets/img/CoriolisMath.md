85ca177a805cbe237e84f41d9229baa0818958d7
Priceisright204 <kprice204@gmail.com>
2020-05-18 12:08:19 -0400

    MAJOR UPDATE
    The main update is removing the article from the model and creating its own site using Jekyll. Also: the model starts with a popup to explain what the page is. There have been numerous minor tweaks and edits to make for a smoother experience.

---
+++ coriolis/assets/img/CoriolisMath.md
@@ -0,0 +1,1 @@
+<iframe src="https://kevindprice.github.io/coriolisMath/CoriolisMath.html" width="100%" height="8000"></iframe>
---
+++ coriolis/serve.bat
@@ -0,0 +1,3 @@
+bundle exec jekyll serve --host 0.0.0.0
+
+REM see https://zarino.co.uk/post/jekyll-local-network/ for the --host part
