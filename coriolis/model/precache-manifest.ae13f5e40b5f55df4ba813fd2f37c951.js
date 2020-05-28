14c306c93fa2141c77edd2b4a753c721f5f82756
Priceisright204 <kprice204@gmail.com>
2020-05-28 19:04:56 -0400

    Update to the coriolis model
    The new model version shifts the page when the menu opens, and uses the ResizeObserver api to measure the canvas sizes when that shift happens. Also, the max height feature is no longer a maximization function, but uses geometry instead.

---
+++ coriolis/model/precache-manifest.ae13f5e40b5f55df4ba813fd2f37c951.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "904589ad8f31a881bede9315a860744d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7425783d56d95ffc4c22",
+    "url": "/static/css/main.ce16e6a1.chunk.css"
+  },
+  {
+    "revision": "74a68d70cf2d22882046",
+    "url": "/static/js/2.a7743483.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a7743483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7425783d56d95ffc4c22",
+    "url": "/static/js/main.9c776952.chunk.js"
+  },
+  {
+    "revision": "47da5885c301c9baae22",
+    "url": "/static/js/runtime-main.f82482ba.js"
+  },
+  {
+    "revision": "244b8ee019defffcbcf30a1ca02d2e52",
+    "url": "/static/media/silhouette.244b8ee0.png"
+  },
+  {
+    "revision": "6a12f96ef2a3f4d14f2b3ac68e31fd48",
+    "url": "/static/media/silhouette2.6a12f96e.png"
+  },
+  {
+    "revision": "0e138312ed65b5ee9014d4df87c89541",
+    "url": "/static/media/stars.0e138312.jpg"
+  }
+]);
