532e16d5fbe41979355ee75fc4008958e6fbaf53
Kevin Price <kprice204@gmail.com>
2024-04-04 12:03:15 -0400

    Massive redesign
    I moved all the subprojects off to Netlify. I also edited a few articles, updated links etc.

--- coriolis/assets/img/CoriolisMath.md
+++
@@ -1,1 +0,0 @@
-<iframe src="https://kevindprice.github.io/coriolisMath/CoriolisMath.html" width="100%" height="8000"></iframe>
--- coriolis/serve.bat
+++
@@ -1,3 +0,0 @@
-bundle exec jekyll serve --host 0.0.0.0
-
-REM see https://zarino.co.uk/post/jekyll-local-network/ for the --host part
